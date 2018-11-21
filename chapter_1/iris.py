94edf4da012f084bf64bb784876aa05cb0300942
tanacchi <q111026d@mail.kyutech.jp>
2018-11-21 23:34:05 +0900

    Upgrade iris introduction code

--- chapter_1/iris.py
+++ chapter_1/iris.py
@@ -86,7 +86,7 @@ Shape of target: (150,)
 
 """
 
-print("Target:\n{}".format(iris_dataset['target']))
+# print("Target:\n{}".format(iris_dataset['target']))
 
 # setona: 0, versicolor: 1, verginia: 2
 """
@@ -99,3 +99,79 @@ Target:
 
 """
 
+# ===================================
+#  Generating training and test data
+# ===================================
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'],
+                                                    iris_dataset['target'],
+                                                    random_state=0)
+
+# print("X_train shape: {}".format(X_train.shape)) # matrix data for training
+# print("y_train shape: {}".format(y_train.shape)) # labels (results) for training
+
+"""
+X_train shape: (112, 4)
+y_train shape: (112,)
+
+"""
+
+
+# print("X_test shape: {}".format(X_test.shape)) # matrix data for test
+# print("y_test shape: {}".format(y_test.shape)) # labels (results) for test
+
+"""
+X_test shape: (38, 4)
+y_test shape: (38,)
+
+"""
+
+
+# ==================
+#  Observation data
+# ==================
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+grr = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+                                 hist_kwds={'bins': 20}, s=60, alpha=.8)
+# plt.show()
+
+
+# =====================
+#  k-Nearest Neighbors
+# =====================
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=1)
+
+knn.fit(X_train, y_train)
+
+
+# ==============
+#  Verification
+# ==============
+
+import numpy as np
+
+y_pred = knn.predict(X_test)
+# print("Test set predictions:\n{}".format(y_pred))
+
+"""
+Test set predictions:
+[2 1 0 2 0 2 0 1 1 1 2 1 1 1 1 0 1 1 0 0 2 1 0 0 2 0 0 1 1 0 2 1 0 2 2 1 0 2]
+
+"""
+
+
+# print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
+
+"""
+Test set score: 0.97
+
+"""
