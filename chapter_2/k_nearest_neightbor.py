f762f50bf05483835a208baa232f3a7ab32f7854
tanacchi <q111026d@mail.kyutech.jp>
2018-12-17 12:38:49 +0900

    Upgrade script for KNN algorithm

--- chapter_2/k_nearest_neightbor.py
+++ chapter_2/k_nearest_neightbor.py
@@ -1,1 +1,12 @@
-import data_set
+# p36
+import mglearn
+from data_set import *
+
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+
+from sklearn.model_selection import train_test_split
+X, y = mglearn.datasets.make_forge()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+
