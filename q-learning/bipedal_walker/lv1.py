a6a0a94732976c7c742db1000693d4b9a12b5c83
tanacchi <q111026d@mail.kyutech.jp>
2019-09-22 15:12:44 +0900

    Add level1 of CartPole and BipedalWalker

---
+++ q-learning/bipedal_walker/lv1.py
@@ -0,0 +1,32 @@
+import gym
+from gym import wrappers
+import time
+
+if __name__ == '__main__':
+    env = gym.make('BipedalWalker-v2')
+    max_steps = 200
+    episodes = 10
+
+    for episode in range(episodes):
+        observation = env.reset()
+        total_reward = 0
+
+        for t in range(max_steps):
+            env.render()
+            time.sleep(0.1)
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                if t + 1 == max_steps:
+                    reward = 100
+                else:
+                    reward = -100
+            else:
+                reward = 1
+            total_reward += reward
+
+            if done:
+                break
+        print("Fin: {}".format(total_reward))
+
+    env.close()
